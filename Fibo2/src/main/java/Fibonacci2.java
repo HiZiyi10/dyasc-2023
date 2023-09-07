3079ffc3c7addb73e9314f1140a1ab043ea1754d
HiZiyi10 <65140270+HiZiyi10@users.noreply.github.com>
2023-09-07 03:48:34 -0300

    Version2 de Fibonacci2.java
    Se agregaron los metodos de "obtenerOpciones", "generarFibonacci", "imprimirFibonacci"

--- Fibo2/src/main/java/Fibonacci2.java
+++ Fibo2/src/main/java/Fibonacci2.java
@@ -4,72 +4,102 @@ import java.util.List;
 
 public class Fibonacci2 {
     public static void main(String[] args) {
-        String orientacion = "h"; // Por defecto, orientación horizontal
-        String direccion = "d";  // Por defecto, dirección directa
+        String orientacion = "h"; // Por defecto, orientacion horizontal
+        String direccion = "d"; // Por defecto, direccion directa
 
-        // Verifico si se proporcionan opciones de orientación y dirección
-        if (args.length >= 1) {
-            for (int i = 0; i < args.length; i++) {
-                if (args[i].equals("-o") && i + 1 < args.length) {
-                    String opcion = args[i + 1];
-                    if (opcion.equals("h") || opcion.equals("v")) {
-                        orientacion = opcion;
-                        i++; // Saltar la siguiente posición que contiene la opción
-                    } else {
-                        System.out.println("Opción no válida: " + opcion);
-                        return;
-                    }
-                } else if (args[i].equals("-d") && i + 1 < args.length) {
-                    String opcion = args[i + 1];
-                    if (opcion.equals("d") || opcion.equals("i")) {
-                        direccion = opcion;
-                        i++; // Saltar la siguiente posición que contiene la opción
-                    } else {
-                        System.out.println("Opción no válida: " + opcion);
-                        return;
-                    }
-                }
-            }
+        // Obtener opciones de orientacion y direccion
+        String[] opciones = obtenerOpciones(args);
+        if (opciones != null) {
+            orientacion = opciones[0];
+            direccion = opciones[1];
+        } else {
+            return; // Ocurrio un error al procesar las opciones, se muestra un mensaje y se sale del programa.
         }
 
         if (args.length >= 1) {
             int n = Integer.parseInt(args[args.length - 1]);
 
             if (n <= 0) {
-                System.out.println("Ingrese un número positivo mayor que cero.");
+                System.out.println("Ingrese un numero positivo mayor que cero.");
                 return;
             }
 
-            List<Integer> fibonacci = new ArrayList<>();
-            fibonacci.add(0);
-            fibonacci.add(1);
+            List<Integer> fibonacci = generarFibonacci(n);
 
-            for (int i = 2; i < n; i++) {
-                int next = fibonacci.get(i - 1) + fibonacci.get(i - 2);
-                fibonacci.add(next);
-            }
-
-            // Si la dirección es "i" (inversa), invertir la lista
+            // Si la direccion es "i" (inversa), invertir la lista
             if (direccion.equals("i")) {
                 Collections.reverse(fibonacci);
             }
 
-            System.out.println("Fibonacci <" + n + ">" + orientacion + direccion + ":");
-            
-            if (orientacion.equals("h")) {
-                // Orientación horizontal
-                for (int num : fibonacci) {
-                    System.out.print(num + " ");
-                }
-            } else if (orientacion.equals("v")) {
-                // Orientación vertical
-                for (int num : fibonacci) {
-                    System.out.println(num);
+            imprimirFibonacci(fibonacci, orientacion,n);
+        } else {
+            System.out.println("Uso: java Fibonacci [-o <orientacion>] [-d <direccion>] <numero>"); // Sale este msj cuando no se ingreso nada
+        }
+    } // Cierro Main
+
+    public static String[] obtenerOpciones(String[] args) {
+        String orientacion = "h"; // Por defecto, orientacion horizontal
+        String direccion = "d"; // Por defecto, direccion directa
+
+        // Verifico si se proporcionaron opciones de orientacion y direccion
+        if (args.length >= 1) {
+            for (int i = 0; i < args.length; i++) {
+                if (args[i].equals("-o") && i + 1 < args.length) {
+                    String opcion = args[i + 1];
+                    if (opcion.equals("h") || opcion.equals("v")) {
+                        orientacion = opcion;
+                        i++; // Avanzo a la siguiente posicion que contiene la opcion
+                    } else {
+                        System.out.println("Opcion no valida: " + opcion);
+                        return null; // Devolver null para indicar un error
+                    }
+                } else if (args[i].equals("-d") && i + 1 < args.length) {
+                    String opcion = args[i + 1];
+                    if (opcion.equals("d") || opcion.equals("i")) {
+                        direccion = opcion;
+                        i++; // Saltar a la siguiente posicion que contiene la opcion, osea el i de for empieza en 2, asi leo si se pasaron opciones para direccion
+                    } else {
+                        System.out.println("Opcion no valida: " + opcion);
+                        return null; // Devolver null para indicar un error
+                    }
                 }
             }
-        } else {
-            System.out.println("Uso: java FibonacciApp [-o <orientación>] [-d <dirección>] <número>");
+        }
+
+        // Devolver las opciones INGRESADAS como un vector de Strings
+        return new String[] { orientacion, direccion };
+    }
+
+      // Generar la serie de Fibonacci y almacenarla en una lista
+    private static List<Integer> generarFibonacci(int n) {
+        List<Integer> fibonacci = new ArrayList<>();
+        fibonacci.add(0);
+        fibonacci.add(1);
+
+        for (int i = 2; i < n; i++) {
+            int next = fibonacci.get(i - 1) + fibonacci.get(i - 2);
+            fibonacci.add(next);
+        }
+
+        return fibonacci;
+    }
+
+    // Imprimir la serie de Fibonacci segun la orientacion especificada
+    private static void imprimirFibonacci(List<Integer> fibonacci, String orientacion,int n) {
+        System.out.println("fibo<" + n + ">:");
+        
+        if (orientacion.equals("h")) {
+            // Orientacion horizontal
+            for (int num : fibonacci) {
+                System.out.print(num + " ");
+            }
+        } else if (orientacion.equals("v")) {
+            // Orientacion vertical
+            for (int num : fibonacci) {
+                System.out.println(num);
+            }
         }
     }
 }
+
 
